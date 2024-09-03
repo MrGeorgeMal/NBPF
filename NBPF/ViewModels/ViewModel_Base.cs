b188398ae4699ff03155045153321f3ee86acb5f
mrmag <mrmag@FLANT_HOME>
2024-09-04 05:27:51 +0700

    Добавьте файлы проекта.

---
+++ NBPF/ViewModels/ViewModel_Base.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace NBPF.ViewModels
+{
+    public class ViewModel_Base : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ NBPF/ViewModels/ViewModel_PageSelector.cs
@@ -0,0 +1,17 @@
+﻿using NBPF.Pages;
+using System.Windows.Controls;
+
+namespace NBPF.ViewModels
+{
+    public class ViewModel_PageSelector : ViewModel_Base
+    {
+        public Page PageFrame1 { get; set; } = new Page_InitData();
+        public Page PageFrame2 { get; set; } = new Page_OutputData();
+        public Page PageFrame3 { get; set; } = new Page_Chart();
+
+        public ViewModel_PageSelector()
+        {
+
+        }
+    }
+}
